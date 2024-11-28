29b7b7fe31a955723984ba87507fbc33fe04f290
dinesh <dk944176@gmail.com>
2024-11-28 17:40:08 -0500

    updated files

--- classVideoStream.py
+++ classVideoStream.py
@@ -1,28 +1,29 @@
-# Threaded class for performance improvement
 from threading import Thread
 import cv2
-class VideoStream:
-	
-	def __init__(self, src=0):
-		self.stream = cv2.VideoCapture(src)
-		(self.grabbed, self.frame) = self.stream.read()
-		self.stopped = False
-		
-	def start(self):
-		Thread(target=self.update, args=()).start()
-		return self
 
-	def update(self):
-                while True:
-                    
-                    
-                			if self.stopped:
-                				return
-                			(self.grabbed, self.frame) = self.stream.read()
-	
-	def read(self):
-                # Return the latest frame
-		return self.frame
-	
-	def stop(self):
-		self.stopped = True
+class VideoStream:
+    def __init__(self, src=0):
+        # Initialize the video stream source (default is 0 for webcam)
+        self.stream = cv2.VideoCapture(src)
+        (self.grabbed, self.frame) = self.stream.read()
+        self.stopped = False
+
+    def start(self):
+        # Start a thread to update the video stream frames
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+        while True:
+            if self.stopped:
+                # Stop updating if the thread is flagged to stop
+                return
+            (self.grabbed, self.frame) = self.stream.read()
+
+    def read(self):
+        # Return the latest frame
+        return self.frame
+
+    def stop(self):
+        # Flag the thread to stop
+        self.stopped = True
